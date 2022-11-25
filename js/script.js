79815d0404f3785a38685addf4dd9a27344c8c7b
TanianaKur <zxzxzx1974@mail.ru>
2022-11-25 11:43:17 +0900

    Добавила добавление задач

--- js/script.js
+++ js/script.js
@@ -1,41 +1,34 @@
 
 let tasks = [
-   {
-      nomer: 1,
-      title: 'home work',
-      description: 'сделать информатику',
-      finish: 'да',
-   },
-   {
-      nomer: 2,
-      title: 'home works',
-      description: 'сделать литературу',
-      finish: 'нет',
-   }
+
+
 ];
-let out_arr = document.getElementById('out_arr');
 
-tasks.forEach(function (task) {
-   console.log(task)
-   const taskElement = document.createElement('div');
+function renderTasks() {
+   let out_arr = document.getElementById('out_arr');
+   out_arr.innerHTML = '';
 
-   const nomerElement = document.createElement('p');
-   nomerElement.innerHTML = `${task.nomer}`;
+   tasks.forEach(function (task) {
+      const taskElement = document.createElement('div');
 
-   const titleElement = document.createElement('p');
-   titleElement.innerHTML = `${task.title}`;
-   const descriptionElement = document.createElement('p');
-   descriptionElement.innerHTML = `${task.description}`;
-   const finishElement = document.createElement('p');
-   finishElement.innerHTML = `${task.finish}`;
-   taskElement.appendChild(nomerElement);
-   taskElement.appendChild(titleElement);
-   taskElement.appendChild(descriptionElement);
-   taskElement.appendChild(finishElement);
-   out_arr.appendChild(taskElement);
+      const nomerElement = document.createElement('p');
+      nomerElement.innerHTML = task.nomer;
 
-});
+      const titleElement = document.createElement('p');
+      titleElement.innerHTML = task.title;
+      const descriptionElement = document.createElement('p');
+      descriptionElement.innerHTML = task.description;
+      const finishElement = document.createElement('p');
+      finishElement.innerHTML = task.finish;
+      taskElement.appendChild(nomerElement);
+      taskElement.appendChild(titleElement);
+      taskElement.appendChild(descriptionElement);
+      taskElement.appendChild(finishElement);
+      out_arr.appendChild(taskElement);
 
+   });
+};
+renderTasks();
 
 const buttonAdd = document.querySelector('.adding-input');
 function showForm() {
@@ -43,18 +36,46 @@ function showForm() {
    const inputElementTitle = document.createElement('input');
 
    blockForList.appendChild(inputElementTitle);
+   inputElementTitle.id = 'title_id';
    inputElementTitle.className = 'inputClass';
+
    const inputElementDescription = document.createElement('input');
+   inputElementDescription.id = 'description_id';
    inputElementDescription.className = 'inputClass';
    blockForList.appendChild(inputElementDescription);
    const saveButton = document.createElement('button');
+   saveButton.className = 'buttonClassAdd';
    saveButton.innerHTML = 'Добавить';
    blockForList.appendChild(saveButton);
+   const buttonAddTask = document.querySelector('.buttonClassAdd');
+
+   saveButton.onclick = addTask
 };
+
+function addTask() {
+   const titleInput = document.getElementById("title_id");
+   const description = document.getElementById("description_id");
+   let adding = {
+      nomer: 1,
+      title: titleInput.value,
+      description: description.value,
+      finish: 'да',
+   }
+   tasks.push(adding);
+
+   renderTasks();
+};
+
 buttonAdd.onclick = showForm;
+
+
+
+
+
+
+
+
 // TODO Домашнее задание:
-// * Добавить стили.
-// * Добавить появление инпутов и кнопки при нажатии кнопки "+"
 // * Добавить создание задачи
 // ** Подумать, как генерировать номер для новой задачи
 // ** Подумать, как добавлять новые задачи и вывод их на форму
